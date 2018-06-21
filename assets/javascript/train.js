440d07880fc4c744d9ab2b938eecd4b2d1993460
unknown <mikeliay@gmail.com>
2018-06-21 18:54:12 -0500

    I can push to the DB

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -16,9 +16,50 @@ var config = {
   console.log(database)
 
 
+var trainName
+var trainDestination
+var trainFrequency
+var firstTrain
 
+// var newTrain = {
+//     name: trainName.value,
+//     destination: trainDestination.value,
+//     frequency: trainFrequency.value
+// }
 
+$("#submit").on("click", function() {
+    event.preventDefault();
+  
+    console.log("Submit is clicked.")
+      trainName = $("#nameSubmit").val().trim()
+      console.log(trainName)
+      trainDestination = $("#destinationSubmit").val().trim()
+      console.log(trainDestination)
+      trainFrequency = $("#frequencySubmit").val().trim()
+      console.log(trainFrequency)
+      firstTrain = $("#firstTrainSubmit").val().trim()
+      console.log(firstTrain)
 
+    //   Ha, this works and is pushing to the DB.
+      database.ref().push({
+          trainName: trainName,
+          trainDestination: trainDestination,
+          trainFrequency: trainFrequency,
+          firstTrain: firstTrain
+          //employeeMonths: employeeMonths
+        //   employeeRate: employeeRate
+        })
+        // This is overwriting the headers
+        // $(cell0).text(trainName)
+        // $("#destinationDisplay").html(trainDestination)
+        // console.log(trainDestination)
+        // $("#frequencyDisplay").html(trainFrequency)
+        // console.log(trainFrequency)
+        // $("#eMonths").html(employeeMonths)
+        // $("#eRate").html(employeeRate)
+  })
+
+//   var cell0 = row.insertCell(0)
 
 
 
