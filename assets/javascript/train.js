43df9ae14e17dfc4e43f3c5e156dfb2906b05636
unknown <mikeliay@gmail.com>
2018-06-22 23:23:58 -0500

    bupkis

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -21,17 +21,12 @@ var trainDestination
 var trainFrequency
 var firstTrain
 
-// var newTrain = {
-//     name: trainName.value,
-//     destination: trainDestination.value,
-//     frequency: trainFrequency.value
-// }
-
 $("#submit").on("click", function() {
     event.preventDefault();
-  
+    
     console.log("Submit is clicked.")
       trainName = $("#nameSubmit").val().trim()
+      $("#nameSubmit").val("")
       console.log(trainName)
       trainDestination = $("#destinationSubmit").val().trim()
       console.log(trainDestination)
@@ -46,23 +41,65 @@ $("#submit").on("click", function() {
           trainDestination: trainDestination,
           trainFrequency: trainFrequency,
           firstTrain: firstTrain
-          //employeeMonths: employeeMonths
-        //   employeeRate: employeeRate
+        //   nextArrival: nextArrival
+        //   minutesAway: minutesAway
+
+        
         })
-        // This is overwriting the headers
-        // $(cell0).text(trainName)
-        // $("#destinationDisplay").html(trainDestination)
-        // console.log(trainDestination)
-        // $("#frequencyDisplay").html(trainFrequency)
-        // console.log(trainFrequency)
-        // $("#eMonths").html(employeeMonths)
-        // $("#eRate").html(employeeRate)
+       // Here it doesn't do anything: return false
+  })
+  
+//   Here it fails: return false
+  database.ref().on("value", function(childSnapshot) {
+   
+      console.log(childSnapshot.val())
+      var data = childSnapshot.val()
+      for (var trainData in data) {
+          console.log(trainData)
+        $(".table > tbody").append("<tr><td>" + data[trainData].trainName + "</td><td>" + data[trainData].trainDestination + "</td><td>"+ data[trainData].trainFrequency + "</td><td>" + data[trainData].firstTrain + " mins" + "</td>")
+      }
+      
   })
 
-//   var cell0 = row.insertCell(0)
+//   I've tried:
+// return false and it kept things from populating at all; or just didn't do anything, depending on placement
+// .clear() just threw an error and said it wasn't a function, then doesn't display anything
+// .empty() clears it after Submit
 
 
+// I'm not a Time Lord.
+// I thought this made sense, but it won't do anything.
 
+var parsed = parseInt(trainFrequency = $("#frequencySubmit").val().trim())
+     console.log("parsed: " + parsed)
+
+        var diffTime = moment().diff(moment.unix(parsed), "minutes")
+		var timeRemainder = moment().diff(moment.unix(firstTrain), "minutes") % parsed
+		var minutes = parsed - timeRemainder
+		var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A")
+		
+        // Time testing
+        
+        // This is just logging NaN
+        console.log("minutes: " + minutes)
+        
+        // This is just logging the current time
+        console.log("nextTrainArrival: " + nextTrainArrival)
+        
+        // This is also just logging the current time
+        console.log(moment().format("hh:mm A"))
+        
+        // Just logging NaN
+        console.log("timeRemainder: " + timeRemainder)
+        
+        // Unix time?
+        console.log(moment().format("X"))
+
+        // Also just NaN
+        console.log("diffTime: " + diffTime)
+
+
+// on submit, empty table, then append everything
 
 
 
